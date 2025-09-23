445f376afc7427f5ff9c0aa9a8859ae1ac361422
killian <killian.gascon@ecoles-epsi.net>
2025-09-23 15:39:15 +0200

    Feat: Register
    Register Fully working
    
    Made By: Killian Gascon
    Ref: Dev-Killian

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,7 @@
-import { prisma } from "@/lib/prisma"
+import { PrismaClient } from "@prisma/client" // ⚠️ ou "@/lib/generated/prisma" si tu gardes output
 import { NextResponse } from "next/server"
+
+const prisma = new PrismaClient()
 
 export async function GET() {
     const users = await prisma.utilisateur.findMany()
